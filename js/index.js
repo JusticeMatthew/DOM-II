99ffd62f9002088674600e7fc4618e3c17a155fb
Matthew Justice <matthewajustice@gmail.com>
2020-12-01 15:33:01 -0500

    Establishing branch 5/10 events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+
+// 1 Mouseover
+
+document.querySelector('h1').addEventListener('mouseover', (event => event.target.style.fontSize = '5rem'
+));
+
+// 2 Mouseout
+
+document.querySelector('h1').addEventListener('mouseout', (event => event.target.style.fontSize = ''
+));
+
+// 3 Keydown
+
+document.addEventListener('keydown', (event => 
+    document.querySelector('header').style.background = 'lightgrey'))
+
+// 4 Keyup
+
+document.addEventListener('keyup', (event =>  
+    document.querySelector('header').style.background = ''))
+
+// 5 Load
+
+window.addEventListener('load', (event => {
+    document.querySelector('body').style.background = 'whitesmoke'
+    console.log('test')
+    }
+))
+
+// 6
+
+
+
+// 7
+
+
+// 8
+
+
+// 9
+
+
+// 10
